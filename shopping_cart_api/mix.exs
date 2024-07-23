8a317ecb578c312ad55aaa712e0267e178f6cfb9
Ahmed Ismail <ahmedismail4612@gmail.com>
2024-07-23 17:51:44 +0800

    chore(backend): add Cors as a depndency to the project

--- shopping_cart_api/mix.exs
+++ shopping_cart_api/mix.exs
@@ -41,7 +41,8 @@ defmodule ShoppingCartApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:cors_plug, "~> 2.0" }
     ]
   end
 
