644575ac68189367301a73711c8a43bd00954dbf
Ahmed Ismail <ahmedismail4612@gmail.com>
2024-07-23 18:00:11 +0800

    feat(database): create database add data to the table and seeds it

--- shopping_cart_api/priv/repo/seeds.exs
+++ shopping_cart_api/priv/repo/seeds.exs
@@ -9,3 +9,42 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias ShoppingCartApi.Repo
+alias ShoppingCartApi.Products.Product
+
+products = [
+  %Product{
+    name: "Macbook Pro",
+    description: "The best laptop for developers",
+    price: 2000,
+    quantity: 10
+  },
+  %Product{
+    name: "iPhone 12",
+    description: "The best phone for developers",
+    price: 1000,
+    quantity: 20
+  },
+  %Product{
+    name: "iPad Pro",
+    description: "The best tablet for developers",
+    price: 800,
+    quantity: 50
+  },
+  %Product{
+    name: "Apple Watch",
+    description: "The best watch for developers",
+    price: 500,
+    quantity: 15
+  },
+  %Product{
+    name: "AirPods Pro",
+    description: "The best earbuds for developers",
+    price: 250,
+    quantity: 30
+  },
+]
+
+Enum.each(products, fn product ->
+  Repo.insert!(product)
+end)
