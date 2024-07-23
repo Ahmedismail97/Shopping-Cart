dd2d18307ddfe1ea3f37e9fc40fc1f2c79b98f43
Ahmed Ismail <ahmedismail4612@gmail.com>
2024-07-23 22:28:27 +0800

    edit git ginore and adding some explaination on controller

--- shopping_cart_api/lib/shopping_cart_api_web/controllers/product_controller.ex
+++ shopping_cart_api/lib/shopping_cart_api_web/controllers/product_controller.ex
@@ -6,11 +6,15 @@ defmodule ShoppingCartApiWeb.ProductController do
 
   action_fallback ShoppingCartApiWeb.FallbackController
 
+  # GET /products
+  # This function is used to list all products in the database. It calls the Products.list_products/0 function to get all products from the database and renders the :index template with the products.
   def index(conn, _params) do
     products = Products.list_products()
     render(conn, :index, products: products)
   end
 
+  # POST /products
+  # This function is used to create a new product. It calls the Products.create_product/1 function to create a new product with the given parameters. If the product is created successfully, it renders the :show template with the created product and sets the response status to :created. If the product creation fails, it returns an error response.
   def create(conn, %{"product" => product_params}) do
     with {:ok, %Product{} = product} <- Products.create_product(product_params) do
       conn
@@ -20,11 +24,15 @@ defmodule ShoppingCartApiWeb.ProductController do
     end
   end
 
+  # GET /products/:id
+  # This function is used to get a single product by its id. It calls the Products.get_product!/1 function to get the product with the given id from the database. If the product is found, it renders the :show template with the product. If the product is not found, it returns a 404 error response.
   def show(conn, %{"id" => id}) do
     product = Products.get_product!(id)
     render(conn, :show, product: product)
   end
 
+  # POST /confirm_purchase
+  # This function is used to confirm a purchase. It calls the Products.confirm_purchase/1 function to confirm the purchase with the given items. If the purchase is confirmed successfully, it returns a 204 No Content response. If the purchase confirmation fails, it returns an error response.
   def confirm_purchase(conn, %{"items" => items}) do
     with {:ok, _} <- Products.confirm_purchase(items) do
       send_resp(conn, :no_content, "")
